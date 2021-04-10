0a44f8e7b7ed6c6e5b9aa0238c17071717ead3fd
mayhope2000 <mayhope2000>
2021-04-10 15:58:20 +0000

    final code along

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryTooltip } from 'victory';
+
+
+const earningsData = [
+  {quarter: 1, earnings: 13000, label:"$13,000"},
+  {quarter: 2, earnings: 16500, label:" $16,500" },
+  {quarter: 3, earnings: 14250, label:"$14,250"},
+  {quarter: 4, earnings: 19000, label:"$19,000"}
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
-    </div>
+      <h1>Data Dashboard</h1>
+      <div className="card">
+        <h3>Company Earnings By Quarter</h3>      
+        <VictoryChart
+        domainPadding={20}
+        theme={VictoryTheme.material}> 
+        <VictoryAxis
+          tickValues={[1,2,3,4]} 
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]} />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={(x) => (`$${x /1000} k`)}/>
+        <VictoryBar
+         label={VictoryTooltip}
+         data={earningsData} 
+         x="quarter" 
+         y="earnings" />
+      </VictoryChart>  
+      </div>
+    </div>  
   );
 }
 
